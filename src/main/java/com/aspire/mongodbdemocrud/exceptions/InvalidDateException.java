63a59e8aaeaa525129e73f18047bb3d1dd3e45e0
mildred.zaragoza <mildred.zaragoza@ASPLAPLTM010.Aspiresys.com>
2023-01-18 16:04:17 -0600

    Date validation

--- src/main/java/com/aspire/mongodbdemocrud/exceptions/InvalidDateException.java
+++ src/main/java/com/aspire/mongodbdemocrud/exceptions/InvalidDateException.java
@@ -3,6 +3,12 @@ package com.aspire.mongodbdemocrud.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception to validate a date.
+ * @author Mildred Zaragoza
+ *
+ */
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidDateException extends Exception{
     /**
--- src/main/java/com/aspire/mongodbdemocrud/utils/DateValidator.java
+++ src/main/java/com/aspire/mongodbdemocrud/utils/DateValidator.java
@@ -7,8 +7,21 @@ import org.springframework.stereotype.Service;
 
 import com.aspire.mongodbdemocrud.exceptions.InvalidDateException;
 
+/**
+ * Class to validate the date introduced by the user
+ * @author Mildred Zaragoza
+ *
+ */
+
 @Service
 public class DateValidator {
+	
+	/**
+	 * This method valid the date introduced by the end user.
+	 * @param date in String format
+	 * @return boolean if the date is valid and false if not.
+	 * @throws InvalidDateException when an invalid date is introduced. 
+	 */
 	public boolean validateDate(String date) throws InvalidDateException {
 		try {
 			SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
